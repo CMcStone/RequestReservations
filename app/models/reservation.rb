f6125bb4c75a80812c738a9ea0dc4f7bb9d5ff3a
ltwheeler <ltwheeler@ucdavis.edu>
2012-05-25 11:50:18 -0700

    Implemented status model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,6 @@
 class Reservation < ActiveRecord::Base
   attr_accessible :description, :expiration, :requested, :status_id, :user_id
+  
+  belongs_to :status 
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120525165039) do
+ActiveRecord::Schema.define(:version => 20120525181226) do
 
   create_table "reservations", :force => true do |t|
     t.integer  "user_id"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(:version => 20120525165039) do
     t.integer  "status_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "statuses", :force => true do |t|
+    t.string   "label"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
